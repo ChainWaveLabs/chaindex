b7cd655c43df7a529b4857c43df45ae16f9e6819
Benjamin M. Brown <benjaminbb@gmail.com>
2018-03-06 14:17:35 -0600

    rem more boilerplate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,7 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
 var FixedSupplyToken = artifacts.require('./FixedSupplyToken.sol');
 var Exchange = artifacts.require('./Exchange.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
   deployer.deploy(FixedSupplyToken);
   deployer.deploy(Exchange);
 };
